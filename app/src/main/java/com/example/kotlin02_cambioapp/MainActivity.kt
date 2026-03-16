67cbaaaa2f409c41dc26d3c6555fa3e1726070dd
JoeAustein <joe.austein@gmail.com>
2026-03-16 12:21:29 -0300

    Implementação CambioApp

--- app/src/main/java/com/example/kotlin02_cambioapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin02_cambioapp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.kotlin02_cambioapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Implementacoes:
+        val botaoConverter = findViewById<Button>(R.id.Button01)
+        botaoConverter.setOnClickListener {
+            convert()
+        }
+    }
+
+    fun convert() {
+        val input = findViewById<EditText>(R.id.EditText01)
+        val output = findViewById<TextView>(R.id.TextView02)
+
+        if(input.length() == 0) {
+            Toast.makeText(this, "Forneca algum valor em Dolar!", Toast.LENGTH_SHORT).show()
+            output.text = "R$ 00,00"
+        }
+        else {
+            val dolar = input.text.toString().toDouble()
+            val real = dolar * 5.5
+            output.text = "R$ $real"
         }
     }
 }
